1ab7a49b4cd3958528f4765cda9f5cbc010f34ea
zaynab <161896517+222zaynab222@users.noreply.github.com>
2025-12-11 22:31:43 +0100

    3

---
+++ CarRental.Core/Rapport.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental.Core;
+
+public class Rapport
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
+    public string title { get; set; }
+    public string Content { get; set; }
+}
